5654d85752301ff8181ac54ab93f46abc5a2be4e
sakachenok <akachenoksveta@gmail.com>
2023-10-12 15:27:16 +0300

    Add new method

--- Hello.java
+++ Hello.java
@@ -1,8 +1,10 @@
 class Hello {
  public static void main(String[] args) {
   int b = 2;
-  int a =3;
+  int a = 3;
+  int c = 4;
    System.out.println(b);
    System.out.println(a);
+   System.out.println(c);
   }
  }
